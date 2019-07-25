4dea7949c9df3ce2aade5bf49d002d8346ab045a
mindcrackx <gino.plaumann@gmail.com>
2019-07-25 08:03:30 +0200

    add utf-8 encoding to mysqldb, fix missing stylesheet in Wartung

--- app/Wartung.php
+++ app/Wartung.php
@@ -10,6 +10,7 @@ validate_access("Wartung");
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="../css/mainstyles.css">
     <title>Ausmusterung</title>
 </head>
 <body>
--- mysqldb.php
+++ mysqldb.php
@@ -4,6 +4,7 @@ $mysqli = new mysqli("127.0.0.1", "admin", "adminadmin", "solorob_db");
 if ($mysqli->connect_error){
     echo "Failed to connect to MySQL: (" . $mysqli->connect_error . ") " . $mysqli->connect_error;
 }
+$mysqli->query("set names utf8");
 #echo $mysqli->host_info . "\n";
 
 require_once("solorob_db_helpers/raeume.php");
