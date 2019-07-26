b7e502f902bcfe51a016deb8e718ad9f762ba672
53276708 <felix@FlizzyBook.local>
2019-07-26 08:14:19 +0200

    changed mainpage

--- app/main.php
+++ app/main.php
@@ -1,10 +1,24 @@
 <style>
-	body {
- 		background-image: url("helfendeHand.jpg");
- 		background-color: #cccccc;
-		 background-repeat: no-repeat;
-		background-size: cover;
-	}
+	body{
+				font-family: "Lato", sans-serif;
+				width: 100%
+			}
+        h1 {
+            color: steelblue;
+        }
+        table {
+            border-collapse: collapse;
+            border-style: hidden;
+            border: 0;
+			width: 100%;
+
+        }
+        th, td {
+            text-align: left;
+            padding: 8px;
+            border-bottom: 1px solid #ddd;
+            border: none;
+		}
 </style>
 <?php
 echo"";
@@ -12,13 +26,15 @@ echo"";
 <html>
 	<head>
 	<link rel="shortcut icon" type="image/x-icon" href="../static/favicon.ico">
-		<h1><div align="right"> Herzlich Willkommen in der IT-Ausstattungsverwaltung von Solorob</div></h1>
 	</head>
 	<body>
-		<h3><div align="right">Sollten Sie Hilfe benötigen, sind wir rund um die Uhr für Sie da!</div></h3>
-		<div align="right">Support Telefon: <a href='tel:+49911756650'>0911 / 7 56 65 - 0</a>
+		<h1><div> Herzlich Willkommen in der IT-Ausstattungsverwaltung von Solorob</div></h1>
+		<h3><div>Sollten Sie Hilfe benötigen, sind wir rund um die Uhr für Sie da!</div></h3>
+		<div>Support-Telefon: <a href='tel:+49911756650'>0911 / 7 56 65 - 0</a>
 			<br>
-			e-Mail: <a href='mailto:support@if11i.b3-fuerth.de'>support@if11i.b3-fuerth.de<a/>
+			E-Mail: <a href='mailto:support@if11i.b3-fuerth.de'>support@if11i.b3-fuerth.de<a/>
 		</div>
+		<br><br>
+		<img src="./helfendeHand.jpg" height="75%" align="center"/>
 	</body>
 </html>
